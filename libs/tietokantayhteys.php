d2177f16bf2de2517631cece28285ec26d44bd83
akaiMe9 <anna.k.mertanen@gmail.com>
2018-12-10 14:02:05 +0200

    Update tietokantayhteys.php

--- libs/tietokantayhteys.php
+++ libs/tietokantayhteys.php
@@ -4,7 +4,7 @@ function getTietokantayhteys() {
   
   static $yhteys = null; 
   $tunnus = "km424941";
-  $salasana= "Kantt4r3lliSQL";
+  $salasana= "*************";
   
   if ($yhteys == null) { 
     $yhteys = new PDO("pgsql:host=dbstud2.sis.uta.fi;port=5432;dbname=$tunnus", $tunnus, $salasana);
